0fd711b20e89e7a9793067c982424e1bcbf9b4ef
Nicholas Ho <j.xiong@live.com>
2022-01-31 16:22:21 +0000

    Completed Task 1C

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -8,8 +8,12 @@ geographical data.
 
 from .utils import sorted_by_key  # noqa
 from haversine import haversine
+from .station import MonitoringStation
 
 def stations_by_distance(stations, p):
     return sorted([
         (station, haversine(station.coord, p))
     for station in stations], key=lambda x: x[1])
+
+def stations_within_radius(stations, centre, r):
+    return [station for station in stations if haversine(station.coord, centre) <= r]
--- tests/test_geo.py
+++ tests/test_geo.py
@@ -2,8 +2,9 @@
 
 from random import random
 
-from floodsystem.geo import stations_by_distance
+from floodsystem.geo import stations_by_distance, stations_within_radius
 from floodsystem.station import MonitoringStation
+from haversine import haversine
 
 def test_stations_by_distance():
 
@@ -30,3 +31,26 @@ def test_stations_by_distance():
     assert type(distances[0][1]) == float
     assert type(distances[1][1]) == float
     assert type(distances[2][1]) == float
+
+def test_stations_within_radius():
+
+    centre = (2.0, 2.0)
+    radius = 200
+
+    # Create 3 stations
+    stations = []
+    for i in range(3):
+        s_id = "s-id-" + str(i)
+        m_id = "m-id-" + str(i)
+        label = "station" + str(i)
+        coord = (i*1.5, i*1.5)
+        trange = (-2.3, 3.4445)
+        river = "River X"
+        town = "My Town"
+        stations.append(MonitoringStation(s_id, m_id, label, coord, trange, river, town))
+
+    stations = stations_within_radius(stations, centre, radius)
+
+    assert len(stations) == 2
+    assert stations[0].station_id == 's-id-1'
+    assert stations[1].station_id == 's-id-2'
