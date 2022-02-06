35aad25534abab1c81a2e9effff65c3f0bc45c3a
Tasin S <sayet025.309@gmail.com>
2022-02-06 00:12:02 +0000

    Completed Task 1E - Not matching representative output on website

--- Task1E.py
+++ Task1E.py
@@ -1,7 +1,6 @@
-from itertools import count
-from floodsystem.geo import rivers_with_station,stations_by_river
+from floodsystem.geo import stations_by_river
 from floodsystem.stationdata import build_station_list
-import tkinter
+import itertools
 
 #haversine function - (x1,x2), (y1,y2) - simply computes the distance between them, simple arithmetic calculation
 
@@ -11,67 +10,39 @@ import tkinter
 stations = build_station_list()
 
 
-
-
-
-#print(rivers_with_station(stations))
-#print(stations_by_river(stations))
-
-
-river_stations = {}
-
-
-def func(stations):
+def rivers_by_station_number(stations, N):
+    stations_count = []
+   # river_stations = {}
     
-    for n, station in enumerate(stations_by_river(stations), 0):
-        #If River not present in list - adds River, creates list with first count of unique monitoring station
-        if station['river'] in river_stations.keys:
-            river_stations[station['river']] = [station['Station name']]
+    for n, r_station in enumerate(stations_by_river(stations).items(),0):
+       # river_stations[r_station[0]] = [station.name for station in r_station[1]]  #Redundant -  Dictionary of Rivers, with list of Station names affiliated
+       stations_count.append((r_station[0], len(r_station[1]))) #Tuple containing Number if Stations and River names
+
+    stations_sorted = sorted(stations_count, key = lambda entry: entry[1], reverse= True) #2nd values stores the number of rivers 
+    stations_sorted_dict = {}
+    #print(stations_sorted)
+
+    for ind, station in enumerate(stations_sorted,0):
+       
+        if ind > N:
+            if stations_sorted[ind+1][1] == station[1]:  #If overflowing - due to 'tie' in number of stations
+                stations_sorted_dict[str(station[1])].append(stations_sorted[ind+1][0]) #Adds River entry
+            else:
+                break #Once required number of RIvers obtained/ exceeded - stops accumulating
+
+        elif ind <= N:
+            if str(station[1]) not in stations_sorted_dict.keys():
+                stations_sorted_dict[str(station[1])]  = [station[0]] #Adds River name to dicitonary of Rivers with keys as their number of Monitoring Stations
+            
+            else:
+                stations_sorted_dict[str(station[1])].append(station[0])
+    rs_output = []
     
-        else:  #Given unique River key already exists in the dictionary - appends the station into the list
-            river_stations[station['river']].append[station['Station name']]
+    #Reconversion back into list of tuples: (River, Number of Stations assigned to River)
+    for number, rivers in stations_sorted_dict.items():
+        for r in rivers:
+            rs_output.append((r, int(number)))
 
-    for n,rs in enumerate(river_stations):
-        print(rs)
-        if n > 50:
-            break     
-    return
+    return rs_output
 
-
-#func(stations)
-print(len(stations_by_river(stations)))
-
-
-n = 0
-for station in stations_by_river(stations).items():
-    #river_stations[item] = [s.name for s in r_station]
-
-   
-
-    #print("River name: %s"%item)
-    #print('\n')
-    #print(stations_by_river(stations)[item])
-    #print('\n')
-    print(station[0])
-
-    n+=1
-
-    if n>10:
-        break
-
-
-
-for n, r_station in enumerate(stations_by_river(stations).items(),0):
-    river_stations[r_station[0]] = [station.name for station in r_station[1]]
-
-for i in river_stations:
-    print(i)
-
-
-
-
-
-
-
-#stations_by_river(stations)  - {'river_name': 'string of stations and info'} ---> split by 'Station name' delimiter into a list of stations
-#Count the number of stations of each river - print the highest and lowest counts
+#print(rivers_by_station_number(stations, 9))
