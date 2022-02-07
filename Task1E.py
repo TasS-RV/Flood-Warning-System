12b0473376d7b7f55544571b1f41f3bacfe3a7aa
Tasin S <sayet025.309@gmail.com>
2022-02-07 12:58:18 +0000

    Shorter function written for Task 1E

--- Task1E.py
+++ Task1E.py
@@ -1,4 +1,4 @@
-from floodsystem.geo import rivers_by_station_number
+from floodsystem.geo import rivers_by_station_number, stations_by_river
 from floodsystem.stationdata import build_station_list
 
 
@@ -17,6 +17,42 @@ def runE(stations, N):
 if __name__ == "__main__":
     runE(stations, N)
     print(runE(stations, N))
+
+
+
+
+
+def rivers_by_station_number(stations, N):
+    river_stations = [(i[0], len(i[1])) for i in stations_by_river(stations).items()]
+    river_stations_sorted = sorted(river_stations, key = lambda x: x[1], reverse = True)
+
+    top_rivers =  []
+
+    for n, r_station in enumerate(river_stations_sorted, 0):
+        if n < N-1:
+            top_rivers.append(r_station)
+
+
+
+        elif n >= N-1:
+            if r_station[1] == river_stations_sorted[n+1][1]:
+                top_rivers.append(river_stations_sorted[n+1])
+            
+            elif r_station[1] != river_stations_sorted[n+1][1]:
+                break
+
+    return top_rivers 
+    
+
+print("\n In new function:")
+print(rivers_by_station_number(stations,9))
+    
+    
+    
+    
+    
+    
+
 
 
 
