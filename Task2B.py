0bb861b8ad8489f91627849117ccef4d32e5ee55
Nicholas Ho <j.xiong@live.com>
2022-02-11 19:04:33 +0000

    Updated Task 2B

---
+++ Task2B.py
@@ -0,0 +1,26 @@
+# Copyright (C) 2018 Garth N. Wells
+#
+# SPDX-License-Identifier: MIT
+
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.flood import stations_levels_over_threshold
+
+
+def run():
+    # Build list of stations
+    stations = build_station_list()
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
+    # Check which stations have relative water levels above 0.8
+    tol = 0.8
+
+    stations_over_tol = stations_levels_over_threshold(stations, tol)
+    for station, level in stations_over_tol:
+        print(station.name, level)
+
+
+if __name__ == "__main__":
+    print("*** Task 2A: CUED Part IA Flood Warning System ***")
+    run()
