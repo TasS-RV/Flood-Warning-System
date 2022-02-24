dad74074694fbf4029a468c74f3ba5ef5d1108a8
Tasin S <sayet025.309@gmail.com>
2022-02-24 17:07:32 +0000

    Plotted typical range High and Low lines

--- Task2F.py
+++ Task2F.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mplt
 import datetime
-import math
+import itertools
 
 from pyparsing import col
 from floodsystem.datafetcher import fetch_measure_levels
@@ -23,11 +23,12 @@ from floodsystem.plot import plot_water_levels
 #Must also show the typical range Low and high for each station on the plot () 
 
 
-def plot_water_level_with_fit(station, dates, levels, p):
+def plot_water_level_with_fit(station, dates, levels, p, range_plot = True): #By default choose to plot the typical range High and Low
 
     typical_range = station.typical_range
-
-    if len(levels) == 0 or len(dates) == 0:
+    
+    #Initially due to empty plot stations with anomalous data
+    if len(levels) == 0 or len(dates) == 0: 
         print("Error: Empty Levels and Date data for Station")
     
     else:
@@ -46,30 +47,38 @@ def plot_water_level_with_fit(station, dates, levels, p):
     
         plt.plot(x_dates_shift, levels, color = 'r', label = "Real Data")
         plt.plot(x_dates_shift, poly(x_dates_shift), color = 'b', label = "Best-fit Curve")
-         
 
-        middle_date = x_dates_shift[math.floor(len(x_dates_shift)/2)]
 
-        plt.scatter([middle_date, middle_date], typical_range, color = 'g', s = 10) #Typical Low and High Ranges indicated
-    
+        if range_plot == True:
+            range_dates = np.linspace(x_dates_shift[0], x_dates_shift[-1], len(x_dates_shift))
+            #print(range_dates)
+
+            range_low = list(itertools.repeat(station.typical_range[0],len(x_dates_shift)))
+            range_high = list(itertools.repeat(station.typical_range[1],len(x_dates_shift)))
+            
+            plt.plot(range_dates, range_low, color = 'g', label = "High line") 
+            plt.plot(range_dates, range_high, color = 'g', label = "Low line")     
+            plt.legend()
+       
     #Customising presentation of Graph:
-
         plt.legend()
         plt.xlabel("Days behind from {0}".format(datetime.datetime.utcnow()))
         plt.ylabel("Water level")
     
-        plt.xticks(rotation=0) #Can accomodate days number easily on x-axis
+        plt.xticks(rotation = 0) #Can accomodate days number easily on x-axis
         plt.title(station.name)
 
     # Display plot
         plt.tight_layout()  # This makes sure plot does not cut off date labels
-
         plt.show()
 
     
 
 '''Next steps: plot the high and low range as reference lines and label, instead iof points, use itertools.repeat for this to maintian the same value
 Consider ideas for how 2G could be done?
+
+To prevent negative date object: convert the polyfit X-dates value using timedelta back into datatime objects, the  plotting to prevent negative days issue
+
 '''
 
 
@@ -84,12 +93,12 @@ update_water_levels(stations)
 M = 5 #Selecting 5 stations with hgihest water levels
 level_stations = stations_highest_rel_level(stations, M)
 
-dt = 10 #Number of days for levels history
+dt = 2 #Number of days for levels history
 p = 4 #Selecting the maximum order of the polynomial for curve fitting
 
 for station in level_stations:
     dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
-    plot_water_level_with_fit(station, dates, levels, p)
+    plot_water_level_with_fit(station, dates, levels, p, True)
 
 
 
