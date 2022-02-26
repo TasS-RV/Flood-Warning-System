45b39a86d9aaa350cbb7c88a039d9c6b7029cc3d
Tasin S <sayet025.309@gmail.com>
2022-02-26 23:21:57 +0000

    Completed linearisation algorithm for 2G

--- Task2G.py
+++ Task2G.py
@@ -22,12 +22,20 @@ import numpy as np
 #Use the same range of dates but different functions
 
 
+def risk_threshold(relative_scale):
+    pass
+
+
+
+
+
 #Loops runs once per station: station object, array of levels and timestamps (dates)
 
-def risk_assessment(station, dates, levels, p):
+def risk_assessment(station, dates, levels, p, plot = False):
 
     date_count = mplt.dates.date2num(dates)
-    date_count_shifted = [day - date_count[0] for day in date_count]
+    date_count_shifted = [day - date_count[-1] for day in date_count]
+    date_count_shifted.reverse()
     
     #Value given relative to typical High value: shifted by Low value
     rel_levels  = [level/(station.typical_range[1] - station.typical_range[0]) for level in levels]
@@ -43,59 +51,89 @@ def risk_assessment(station, dates, levels, p):
 
     func = ""
     for expo, c in enumerate(coeff_series, 0): #Last coeff. is constant term, equivalent to x^0
-        func = func + str(c) + "*(x**{})*".format(expo)
-    func = func + "1" #Due to extra * at end
+        func = func + str(c) + "*(x**{})+".format(expo)
+    func = func + "0" #Due to extra  at end
     
     deriv = ""
     #Can apply algorithmic differentiation method for polynomial:
     for expo, c in enumerate(coeff_series[1:], 1): #Last coeff. is constant term, equivalent to x^0
-        deriv = deriv + str(c*expo) + "*(x**{})*".format(expo-1)
-    deriv = deriv + "1"
+        deriv = deriv + str(c*expo) + "*(x**{})+".format(expo-1)
+    deriv = deriv + "0"
+    #Presenting functions for testing:#print("\n Normal function: {}".format(func))#print("Derivative of function: {}".format(deriv))
     
-    #Presenting functions for testing:
-    print("\n Normal function: {}".format(func))
-    print("Derivative of function: {}".format(deriv))
+    
+    #Linearisation:
+    rel_levels_fitted = list(fitted_levels(date_count_shifted))#np.array must be converted into list for append function use
+    x = date_count_shifted[-1] #Present day value
 
+
+    #Pre-linearisation graphs:
+    if plot == True:
     #[eval(deriv) for x in rel_levels] generates a list of values
-    plt.plot(date_count_shifted, rel_levels, color = 'b', label = "True values")
-    plt.plot(date_count_shifted, fitted_levels, color = 'r', label = "Fitted values") #Values with polynomial
-  #  x = date_count_shifted[-1]+1 #Next day increment
-   # date_count_shifted.append(x)
+        plt.plot(date_count_shifted, rel_levels, color = 'b', label = "True values")
+        plt.plot(date_count_shifted, rel_levels_fitted, color = 'r', label = "Fitted values") #Values with polynomial
+
+
+    #1/4 day value
+    quarter_day = rel_levels_fitted[-1] + eval(deriv)*0.25
+    date_count_shifted.append(x+0.25)
+    #1/2 Day value
+    half_day = rel_levels_fitted[-1] + eval(deriv)*0.5
+    date_count_shifted.append(x+0.5)
+
+    rel_levels_fitted.append(quarter_day) #Using rate of change at present 
+    rel_levels_fitted.append(half_day)
+     
+    #Graphical display of predicted water level:
+    if plot == True:
+        plt.plot(date_count_shifted, rel_levels_fitted, color = 'g', label = "Linearised next value") #Values with polynomial 
+    # plt.plot(date_count_shifted, [eval(deriv) for x in rel_levels], color = 'o', label = "First derivative of fitted polynomial")
+        plt.xlabel("Days TO present from 3 days ago")
+        plt.ylabel("Relative water level to typical range values.")
     
-    #.append(fitted_levels[-1]+1*int(eval(deriv))) #Linearised next value of water level 
+        plt.title(station.town)
+        plt.legend()
+        plt.show()
 
-  #  plt.plot(date_count_shifted, fitted_levels, color = 'r') #Values with polynomial 
-   # plt.plot(date_count_shifted, [eval(deriv) for x in rel_levels])
+       
 
-    plt.legend()
-    plt.show()
+    #Scaling factors based on relative importance of present (ps) and quarter day (fs1) and half day (fs2)
+    ps = 0.55
+    fs1 = 0.33 
+    fs2 = 0.12
+    
+    relative_scale = ps*rel_levels_fitted[-3] + fs1*quarter_day + fs2*half_day
+    
+    return risk_threshold(relative_scale)
 
-    #Scaling factors based on relative importance of present (ps) and tomorrow's level data (fs) 
-    ps = 0.6
-    fs = 0.4 
 
     
 
 
 
-def task_run():    
+def task_run(show_plot):    
     stations =  build_station_list()
 
     # Update latest level data for all stations (to present time))
     update_water_levels(stations)
 
+    M = 5 #Selecting 5 stations with hgihest water levels
+    high_stations = stations_highest_rel_level(stations, M)
+
+    
     dt = 3 #Number of days for levels history - too high and polyfit may not be accurate, too low and insufficient points
     p = 4 #Selecting the maximum order of the polynomial for curve fitting
     
-    #Dictioanry of towns followed by numerical risk scaling (discrete)
+    #Dictioanary of towns followed by numerical risk scaling (discrete)
     risk_level = {}
 
-    for station in stations:
+    for station in high_stations:
         dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
   
-        risk_level[station.town] = risk_assessment(station, dates, levels, p)
+        risk_level[station.town] = risk_assessment(station, dates, levels, p, show_plot)
 
 
 
 if __name__ == "__main__":
-    task_run()
+    show_plot = True
+    task_run(show_plot)
