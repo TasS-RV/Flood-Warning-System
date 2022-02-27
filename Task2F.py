155ec1fc1090806735d8f128ca85cc20f0d50c74
Nicholas Ho <j.xiong@live.com>
2022-02-27 20:44:45 +0000

    Formatting changes

--- Task2F.py
+++ Task2F.py
@@ -13,12 +13,6 @@ from floodsystem.datafetcher import fetch_measure_levels
 from floodsystem.flood import stations_highest_rel_level
 from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.plot import plot_water_levels
-
-
-
-
-
-
     
 #Must also show the typical range Low and high for each station on the plot () 
 
