279f968194de71330e5e32e72d299fd301eda439
Tasin S <sayet025.309@gmail.com>
2022-02-27 01:20:57 +0000

    Not causing at issue in Datafetcher

--- Task2G.py
+++ Task2G.py
@@ -35,80 +35,74 @@ def risk_assessment(station, dates, levels, p, plot = False):
     date_count_shifted = [day - date_count[-1] for day in date_count]
     date_count_shifted.reverse()
     
-    if levels != None: #Checking for non-empty list
 
-        try:
+    #Value given relative to typical High value: shifted by Low value
+    rel_levels  = [level/(station.typical_range[1] - station.typical_range[0]) for level in levels]
+    
+    coeff = np.polyfit(date_count_shifted, rel_levels, p)  #Coefficient finding for fitting level and dates data with polynomial or degree p
+    fitted_levels = np.poly1d(coeff) #Y-values for polynomial fit relative to correcponding x values of date_count_shifted
 
+    #Linearisation performed with time-step 1 (equivalent to 1 day)
+    #eval(.format((date_count_shifted[-1]+1),))
 
-                
-            #Value given relative to typical High value: shifted by Low value
-            rel_levels  = [level/(station.typical_range[1] - station.typical_range[0]) for level in levels]
-            
-            coeff = np.polyfit(date_count_shifted, rel_levels, p)  #Coefficient finding for fitting level and dates data with polynomial or degree p
-            fitted_levels = np.poly1d(coeff) #Y-values for polynomial fit relative to correcponding x values of date_count_shifted
+    coeff_series = [c for c in coeff]
+    coeff_series.reverse() #To reverse in order of increasing exponents
 
-            #Linearisation performed with time-step 1 (equivalent to 1 day)
-            #eval(.format((date_count_shifted[-1]+1),))
-
-            coeff_series = [c for c in coeff]
-            coeff_series.reverse() #To reverse in order of increasing exponents
-
-            func = ""
-            for expo, c in enumerate(coeff_series, 0): #Last coeff. is constant term, equivalent to x^0
-                func = func + str(c) + "*(x**{})+".format(expo)
-            func = func + "0" #Due to extra  at end
-            
-            deriv = ""
-            #Can apply algorithmic differentiation method for polynomial:
-            for expo, c in enumerate(coeff_series[1:], 1): #Last coeff. is constant term, equivalent to x^0
-                deriv = deriv + str(c*expo) + "*(x**{})+".format(expo-1)
-            deriv = deriv + "0"
-            #Presenting functions for testing:#print("\n Normal function: {}".format(func))#print("Derivative of function: {}".format(deriv))
-            
-            
-            #Linearisation:
-            rel_levels_fitted = list(fitted_levels(date_count_shifted))#np.array must be converted into list for append function use
-            x = date_count_shifted[-1] #Present day value
+    func = ""
+    for expo, c in enumerate(coeff_series, 0): #Last coeff. is constant term, equivalent to x^0
+        func = func + str(c) + "*(x**{})+".format(expo)
+    func = func + "0" #Due to extra  at end
+    
+    deriv = ""
+    #Can apply algorithmic differentiation method for polynomial:
+    for expo, c in enumerate(coeff_series[1:], 1): #Last coeff. is constant term, equivalent to x^0
+        deriv = deriv + str(c*expo) + "*(x**{})+".format(expo-1)
+    deriv = deriv + "0"
+    #Presenting functions for testing:#print("\n Normal function: {}".format(func))#print("Derivative of function: {}".format(deriv))
+    
+    
+    #Linearisation:
+    rel_levels_fitted = list(fitted_levels(date_count_shifted))#np.array must be converted into list for append function use
+    x = date_count_shifted[-1] #Present day value
 
 
-            #Pre-linearisation graphs:
-            if plot == True:
-            #[eval(deriv) for x in rel_levels] generates a list of values
-                plt.plot(date_count_shifted, rel_levels, color = 'b', label = "True values")
-                plt.plot(date_count_shifted, rel_levels_fitted, color = 'r', label = "Fitted values") #Values with polynomial
+    #Pre-linearisation graphs:
+    if plot == True:
+    #[eval(deriv) for x in rel_levels] generates a list of values
+        plt.plot(date_count_shifted, rel_levels, color = 'b', label = "True values")
+        plt.plot(date_count_shifted, rel_levels_fitted, color = 'r', label = "Fitted values") #Values with polynomial
 
 
-            #1/4 day value
-            quarter_day = rel_levels_fitted[-1] + eval(deriv)*0.25
-            date_count_shifted.append(x+0.25)
-            #1/2 Day value
-            half_day = rel_levels_fitted[-1] + eval(deriv)*0.5
-            date_count_shifted.append(x+0.5)
+    #1/4 day value
+    quarter_day = rel_levels_fitted[-1] + eval(deriv)*0.25
+    date_count_shifted.append(x+0.25)
+    #1/2 Day value
+    half_day = rel_levels_fitted[-1] + eval(deriv)*0.5
+    date_count_shifted.append(x+0.5)
 
-            rel_levels_fitted.append(quarter_day) #Using rate of change at present 
-            rel_levels_fitted.append(half_day)
-            
-            #Graphical display of predicted water level:
-            if plot == True:
-                plt.plot(date_count_shifted, rel_levels_fitted, color = 'g', label = "Linearised next value") #Values with polynomial 
-            # plt.plot(date_count_shifted, [eval(deriv) for x in rel_levels], color = 'o', label = "First derivative of fitted polynomial")
-                plt.xlabel("Days TO present from 3 days ago")
-                plt.ylabel("Relative water level to typical range values.")
-            
-                plt.title(station.town)
-                plt.legend()
-                plt.show()
+    rel_levels_fitted.append(quarter_day) #Using rate of change at present 
+    rel_levels_fitted.append(half_day)
+    
+    #Graphical display of predicted water level:
+    if plot == True:
+        plt.plot(date_count_shifted, rel_levels_fitted, color = 'g', label = "Linearised next value") #Values with polynomial 
+    # plt.plot(date_count_shifted, [eval(deriv) for x in rel_levels], color = 'o', label = "First derivative of fitted polynomial")
+        plt.xlabel("Days TO present from 3 days ago")
+        plt.ylabel("Relative water level to typical range values.")
+    
+        plt.title(station.town)
+        plt.legend()
+        plt.show()
 
-            #Scaling factors based on relative importance of present (ps) and quarter day (fs1) and half day (fs2)
-            ps = 0.55
-            fs1 = 0.33 
-            fs2 = 0.12
-            
-            relative_scale = ps*rel_levels_fitted[-3] + fs1*quarter_day + fs2*half_day
-            return risk_threshold(relative_scale)
-        
-        except Exception:
-            print(station, levels, dates)
+    #Scaling factors based on relative importance of present (ps) and quarter day (fs1) and half day (fs2)
+    ps = 0.55
+    fs1 = 0.33 
+    fs2 = 0.12
+    
+    relative_scale = ps*rel_levels_fitted[-3] + fs1*quarter_day + fs2*half_day
+    return risk_threshold(relative_scale)
+    
+
 
 
 
@@ -117,35 +111,50 @@ def risk_assessment(station, dates, levels, p, plot = False):
 def task_run(show_plot):    
     stations =  build_station_list()
 
-    consistent_stations = [station for station in stations if station.name not in inconsistent_typical_range_stations(stations)]
-     
-    #Reusing this as invalid stations are prefiltered out
-    M = len(stations)
-    consis_stations = stations_highest_rel_level(stations, M)
-   
     # Update latest level data for all stations (to present time))
     update_water_levels(stations)
     
-    #Uncomment out for refernce to typical scales of Highest and Lowest Water level stations
-   # M = 5 #Selecting 5 stations with hgihest water levels
-    #high_stations = stations_highest_rel_level(stations, M)
+    consistent_stations = [station for station in stations if station not in inconsistent_typical_range_stations(stations)]
 
+  #  print(consistent_stations)
     
     dt = 3 #Number of days for levels history - too high and polyfit may not be accurate, too low and insufficient points
-    p = 4 #Selecting the maximum order of the polynomial for curve fitting
+    p = 6 #Selecting the maximum order of the polynomial for curve fitting
     
-    #Dictioanary of towns followed by numerical risk scaling (discrete)
+    #Dictionary of towns followed by numerical risk scaling (discrete)
     risk_level = {}
 
-    for station in consis_stations:
-        dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
-        risk_level[station.town] = risk_assessment(station, dates, levels, p, show_plot)
-        
-        if risk_level[station.town] == None:
+    for n, station in enumerate(consistent_stations, 0):
+        try:
+            dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
+            if levels != None and len(dates) > 0:     
+                  
+                try:
+                   
+                    risk_level[station.town] = risk_assessment(station, dates, levels, p, show_plot)
+                
+                except Exception:
+                    print("Tripped in function") 
+
+                    print(station.name)
+                    print(levels)
+                    print(dates)
+                    print("Length of values levels{} dates {}".format(len(levels), len(dates)))
+                    print("\n\n\n\n")
+                    print(station)
+
+        except Exception:
+            print("Tripped at datafetcher")
+
             print(station.name)
             print(levels)
             print(dates)
+            print("Length of values levels{} dates {}".format(len(levels), len(dates)))
+            print("\n\n\n\n")
+            print(station)
+
         
+            
        # print(risk_level)
     
         
